55afc7d46f2b9e9c9491c17c7e0b534a8afb2cee
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-02-24 17:22:22 +0100

    navbar + banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { navbar as Navbar } from './components/navbar/index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { banner as Banner } from './components/heroBanner/banner.js';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div>
+			<Navbar></Navbar>
+			<Banner></Banner>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/heroBanner/banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const banner = () => {
+	return (
+		<div>
+			<section class='hero-banner'>
+				<div class='container text-center'>
+					<span class='hero-banner-icon'>
+						<i class='flaticon-sing'></i>
+					</span>
+					<p>08 - 11 April 2021, Hammamet City</p>
+					<h1>CSTC 2021</h1>
+					<a class='button button-header' href='#'>
+						About CSTC
+					</a>
+				</div>
+			</section>
+		</div>
+	);
+};
