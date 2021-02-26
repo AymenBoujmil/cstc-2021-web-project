3f32c276542795c920fe52c91a46c60f787608ad
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-02-26 13:18:44 +0100

    schedule_finished

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Day1 } from './day1';
+import data1 from '../../_utils/schedule/day1.json';
+import data2 from '../../_utils/schedule/day2.json';
+import data3 from '../../_utils/schedule/day3.json';
 
 export const Schedule = () => {
+	const [day, setDay] = useState(data1.day1);
+	let affiche = () => {
+		console.log(day);
+	};
+	const changeDay = (e) => {
+		console.log(e.target.parentNode);
+		let a = e.target;
+		if (e.target.nodeName != 'A') {
+			a = e.target.parentNode;
+		}
+		let dayV = a.getAttribute('href').slice(1, 5);
+		console.log(dayV);
+
+		switch (dayV) {
+			case 'day1':
+				setDay(data1.day1);
+				break;
+			case 'day2':
+				setDay(data2.day2);
+				break;
+			case 'day3':
+				setDay(data3.day3);
+				break;
+		}
+	};
 	return (
 		<div>
 			<section class='section-margin mb-5 pb-5'>
@@ -16,19 +44,24 @@ export const Schedule = () => {
 							<div class='scheduleTab'>
 								<ul class='nav nav-tabs'>
 									<li class='nav-item text-center'>
-										<a data-toggle='tab' href='#day1'>
+										<a
+											className='active'
+											data-toggle='tab'
+											href='#day1'
+											onClick={changeDay}
+										>
 											<h4>Day 1</h4>
 											<p>23 April, 2021</p>
 										</a>
 									</li>
 									<li class='nav-item text-center'>
-										<a class='active' data-toggle='tab' href='#day2'>
+										<a data-toggle='tab' href='#day2' onClick={changeDay}>
 											<h4>Day 2</h4>
 											<p>24 April, 2021</p>
 										</a>
 									</li>
 									<li class='nav-item text-center'>
-										<a data-toggle='tab' href='#day3'>
+										<a data-toggle='tab' href='#day3' onClick={changeDay}>
 											<h4>Day 3</h4>
 											<p>25 April, 2021</p>
 										</a>
@@ -39,7 +72,7 @@ export const Schedule = () => {
 					</div>
 
 					<div class='tab-content'>
-						<Day1></Day1>
+						<Day1 day={day} affiche={affiche}></Day1>
 					</div>
 				</div>
 			</section>
--- src/components/schedule/day1.js
+++ src/components/schedule/day1.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import data from '../../_utils/schedule/day1.json';
+// import data from '../../_utils/schedule/day1.json';
 import { Schedule } from './Schedule';
+import { Schedule_card } from './schedule-card';
 
-export const Day1 = () => {
-	const [day1, setday1] = useState(data.day1);
+export const Day1 = (props) => {
+	// const [day1, setday1] = useState(data.day1);
 
 	return (
 		<div>
 			<div id='day1' class='tab-pane'>
 				{' '}
-				{day1.map((card) => (
-					<Schedule day={day1}></Schedule>
+				{props.day.map((card) => (
+					
+					<Schedule_card day={card} affiche={props.affiche}></Schedule_card>
 				))}{' '}
+				{/* <p onClick={props.affiche}> affiche</p> */}
 			</div>
 		</div>
 	);
