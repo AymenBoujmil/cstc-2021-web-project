db098445a5de3742c51e844a63210f04f4c6eeef
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-03-06 16:57:37 +0100

    aboutUs-modified

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './aboutUs.css';
+
 import { IoIosClock } from 'react-icons/io';
+import { ImBullhorn } from 'react-icons/im';
+
+import { GiMicrophone } from 'react-icons/gi';
+import { IoRocket } from 'react-icons/io5';
 
 const primary_text = {
 	color: '#f50136',
 };
+
 export const AboutUs = () => {
+	const onStyle = {
+		padding: '0 10px',
+		fontSize: '72px',
+		verticalAlign: 'middle',
+		color: '#f50136',
+		borderRadius: '4px',
+		display: 'block',
+		marginBottom: '-2px',
+		lineHeight: '1',
+		WebkitTransition: 'all 0.4s ease-in-out',
+		transition: 'all 0.4s ease-in-out',
+	};
+	const outStyle = {
+		padding: '0 10px',
+		fontSize: '72px',
+		verticalAlign: 'middle',
+		color: '#fff',
+		borderRadius: '4px',
+		display: 'block',
+		marginBottom: '-2px',
+		lineHeight: '1',
+		WebkitTransition: 'all 0.4s ease-in-out',
+		transition: 'all 0.4s ease-in-out',
+	};
+	const [styleAlarm, setStyleClock] = useState(onStyle);
+	const [styleBullhorn, setstyleBull] = useState(onStyle);
+	const [styleRocket, setstyleRocket] = useState(onStyle);
+	const [styleMic, setstyleMic] = useState(onStyle);
+	const onMouseOver = () => {
+		setStyleClock(onStyle);
+	};
+	const onMouseOut = () => {
+		setStyleClock(outStyle);
+	};
+	const onMouseOverBull = () => {
+		setstyleBull(onStyle);
+	};
+	const onMouseOutBull = () => {
+		setstyleBull(outStyle);
+	};
+	const onMouseOverRocket = () => {
+		setstyleRocket(onStyle);
+	};
+	const onMouseOutrocket = () => {
+		setstyleRocket(outStyle);
+	};
+	const onMouseOverMic = () => {
+		setstyleMic(onStyle);
+	};
+	const onMouseOutMic = () => {
+		setstyleMic(outStyle);
+	};
+
 	return (
 		<div>
 			<section className='pt100 pb100'>
@@ -38,8 +97,12 @@ export const AboutUs = () => {
 
 					<div className='row justify-content-center mt30'>
 						<div className='col-12 col-md-6 col-lg-3'>
-							<div className='icon_box_one'>
-								<i className='fas fa-microphone'></i>
+							<div
+								className='icon_box_one'
+								onMouseOut={onMouseOverMic}
+								onMouseOver={onMouseOutMic}
+							>
+								<GiMicrophone size='120px' style={styleMic} />{' '}
 								<div className='content'>
 									<h4>9 Speakers</h4>
 									<p className='aboutUsP'>
@@ -50,8 +113,12 @@ export const AboutUs = () => {
 							</div>
 						</div>
 						<div className='col-12 col-md-6 col-lg-3'>
-							<div className='icon_box_one'>
-								<i className='fas fa-rocket'></i>{' '}
+							<div
+								className='icon_box_one'
+								onMouseOut={onMouseOverRocket}
+								onMouseOver={onMouseOutrocket}
+							>
+								<IoRocket size='120px' style={styleRocket}></IoRocket>{' '}
 								<div className='content'>
 									<h4>8 hrs Marathon</h4>
 									<p className='aboutUsP'>
@@ -62,8 +129,12 @@ export const AboutUs = () => {
 							</div>
 						</div>
 						<div className='col-12 col-md-6 col-lg-3'>
-							<div className='icon_box_one'>
-								<i className='fas fa-bullhorn'></i>{' '}
+							<div
+								className='icon_box_one'
+								onMouseOut={onMouseOverBull}
+								onMouseOver={onMouseOutBull}
+							>
+								<ImBullhorn size='120px' style={styleBullhorn}></ImBullhorn>
 								<div className='content'>
 									<h4>Live Broadcast</h4>
 									<p className='aboutUsP'>
@@ -73,21 +144,14 @@ export const AboutUs = () => {
 								</div>
 							</div>
 						</div>
+
 						<div className='col-12 col-md-6 col-lg-3'>
-							<div className='icon_box_one'>
-								<i className='far fa-clock'></i>{' '}
-								<div className='content'>
-									<h4>Early Bird</h4>
-									<p className='aboutUsP'>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-										rhoncus massa nec graviante.
-									</p>
-								</div>
-							</div>
-						</div>
-						<div className='col-12 col-md-6 col-lg-3'>
-							<div className='icon_box_one'>
-								<i className='far fa-clock'></i>{' '}
+							<div
+								className='icon_box_one'
+								onMouseOut={onMouseOver}
+								onMouseOver={onMouseOut}
+							>
+								<IoIosClock size='120px' style={styleAlarm}></IoIosClock>
 								<div className='content'>
 									<h4>Early Bird</h4>
 									<p className='aboutUsP'>
