8022ad43ebbc9c366a2da02c265e2ad8bc61b88a
aminfelah <60293387+aminfelah@users.noreply.github.com>
2021-04-11 22:24:26 +0200

    testing contact us

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 
 
 export const ContactUs = () => {
-  
+
   const { register, errors, handleSubmit, reset } = useForm();
   const toastifySuccess = () => {
     toast('Form sent!', {
@@ -25,19 +25,23 @@ export const ContactUs = () => {
 
 
   const onSubmit = async (data) => {
-	console.log(data);
+    console.log(data);
+    console.log('name ', data.name);
+    console.log('object', data.object)
     console.log('Email: ', data.email);
-	console.log('Message: ', data.message);
+    console.log('Message: ', data.message);
 
-	  try {
+    try {
       const templateParams = {
+        name: data.name,
+        object: data.object,
         email: data.email,
         message: data.message
       };
 
       await emailjs.send(
-       	"service_8zvmigg",
-        "template_h9lhwq6",
+        "service_k26tw38",
+        "template_xfnqa5i",
         templateParams,
         "user_HYwquAFDNWv3tt1tP6xPZ"
       );
@@ -50,56 +54,54 @@ export const ContactUs = () => {
   };
 
 
-        return (
-        <div>    
-        <div class="container">
-              <div class="section-intro text-center pb-80px">
-                <h3 class="primary-text"> ContactUS </h3> 
-              </div>
-            </div>
-            	<div class="bg-contact100" >
-		            <div class="container-contact100">
-			            <div class="wrap-contact100">
-				            <div class="contact100-pic js-tilt" data-tilt>
-				        	    <img src="image/ContactUs/ContactMail.png" alt="IMG"></img>
-		        	    	</div>
-                           
-
-				           <form class="contact100-form validate-form"  id='contact-form' onSubmit={handleSubmit(onSubmit)}>
-                                <span class="contact100-form-title">
-									<h2>	<i class="fas fa-envelope-open-text"></i> Email Us</h2>
-								</span>
-
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name='email' placeholder="Email" ref={register} /> 
-						    <span class="focus-input100"></span>
-					       
-                           
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Message is required">
-						<textarea class="input100" name='message' placeholder="Message"  ref={register} />
-						<span class="focus-input100">
-						
-						</span>
-                       
-					</div>
-
-
-					<div class="container-contact100-form-btn">
-         
-						<button class="contact100-form-btn" type='submit'>
-							Send
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
+  return (
+    <div>
+      <div class="container">
+        <div class="section-intro text-center pb-80px">
+          <h3 class="primary-text"> Contact Us</h3>
         </div>
-        
-        
-        );
-    }
+      </div>
+      <div class="bg-contact100" >
+        <div class="container-contact100">
+          <div class="wrap-contact100">
+            <div class="contact100-pic js-tilt" data-tilt>
+              <img src="image/ContactUs/ContactMail.png" alt="IMG"></img>
+            </div>
+
+
+            <form class="contact100-form validate-form" id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+              <span class="contact100-form-title">
+                <h2>	<i class="fas fa-envelope-open-text"></i> Email Us</h2>
+              </span>
+              <div class="wrap-input100 validate-input" data-validate="name is required">
+                <input class="input100" type="text" name='name' placeholder="name" ref={register} />
+              </div>
+              <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                <input class="input100" type="text" name='email' placeholder="Email" ref={register} />
+              </div>
+              <div class="wrap-input100 validate-input" data-validate="object is required">
+                <input class="input100" type="text" name='object' placeholder="object" ref={register} />
+              </div>
+
+              <div class="wrap-input100 validate-input" data-validate="Message is required">
+                <textarea class="input100" name='message' placeholder="Message" ref={register} />
+              </div>
+
+
+              <div class="container-contact100-form-btn">
+
+                <button class="contact100-form-btn" type='submit'>
+                  Send
+						</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+  );
+}
 
