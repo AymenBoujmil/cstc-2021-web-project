4808930859e7946d593f1b7720aa1ce907e1169e
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-04-19 18:20:28 +0100

    retoucheet background _  home colors

--- src/components/heroBanner/banner.js
+++ src/components/heroBanner/banner.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { Countdown } from '../countdown/countdown';
 export const banner = () => {
 	return (
-		<div id="home">
+		<div id='home'>
 			<section className='hero-banner'>
+				<div class='overlay'></div>
+
 				<div className='container text-center'>
-					
-					<div className="around">
+					<div className='around'>
 						<p>08 - 11 April 2021, Hammamet City</p>
-					<h1>CSTC 2021</h1>
-					<Countdown></Countdown>
-					<a className='button button-header' href='#'>
-						About CSTC
-					</a>
+						<h1>CSTC 2021</h1>
+						<Countdown></Countdown>
 					</div>
-					
 				</div>
 			</section>
 		</div>
