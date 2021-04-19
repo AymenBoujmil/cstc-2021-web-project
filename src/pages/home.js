4a4fe620ddc9ae78ca7345a4b88113c4abda398c
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-04-19 17:39:38 +0100

    team

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { banner as Banner } from '../components/heroBanner/banner';
 
-
 import { AboutUs } from '../components/AboutUs/AboutUs';
 import { ContactUs } from './ContactUs/ContactUs';
 import { Sponsor } from '../components/sponsor/sponsor';
-import { Team } from '../components/Team/Team';
+import Team from '../components/Team/team2';
 
 export default function Home() {
 	return (
