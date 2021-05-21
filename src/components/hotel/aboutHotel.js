f21926a2e78b8efb1d25cc9e47e66eae64f101ab
aminfelah <60293387+aminfelah@users.noreply.github.com>
2021-05-21 22:23:19 +0200

    hotel carousel

--- src/components/hotel/aboutHotel.js
+++ src/components/hotel/aboutHotel.js
@@ -4,7 +4,8 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import data from '../../_utils/hotel/hotel.json';
 import OwlCarousel from 'react-owl-carousel';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 export const AboutHotel = () => {
 	const [hotel, setHotel] = useState(data.Hotel);
 	return (
@@ -14,13 +15,24 @@ export const AboutHotel = () => {
 					About the hotel
 				</h3>
 			</div>
-			<OwlCarousel className='owl-theme' loop margin={10}>
+			{/*<OwlCarousel className='owl-theme' loop margin={10}>
 				{hotel.map((img, index) => (
 					<div class='item' key={index}>
 						<img src={img.image} alt='' />
 					</div>
 				))}
-			</OwlCarousel>
+			</OwlCarousel> */}
+			<div style={{height:"75%",width:"75%",textAlign:"center",margin:"auto"}}>
+			 <Carousel autoPlay infiniteLoop dynamicHeight>
+			 
+			 {hotel.map((img, index) => (
+					<div class='item' key={index}>
+						<img src={img.image} alt='' />
+					</div>
+				))}
+		 </Carousel>
+			</div>
 		</div>
+		
 	);
 };
